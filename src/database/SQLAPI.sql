6f0b07941bcc40a231f748f9258ba3b0e0f27a23
henriqferreira <henriquepfn@gmail.com>
2022-05-25 10:18:01 -0300

    novo banco de dados

--- src/database/SQLAPI.sql
+++ src/database/SQLAPI.sql
@@ -1,35 +1,16 @@
 create database API_Codificados;
 use API_Codificados;
 
-CREATE TABLE Administrador(
-    idAdm INT NOT NULL AUTO_INCREMENT,
-    adm_email VARCHAR(100) NOT NULL,
-    adm_senha VARCHAR(50) NOT NULL,
-    adm_tecnico_index INT NOT NULL,
-    primary key(idAdm)
-);
-
-INSERT INTO Administrador(adm_email, adm_senha, adm_tecnico_index) VALUES('admin', 'admin', 1);
-
 CREATE TABLE Usuarios(
-	idUsuario INT NOT NULL AUTO_INCREMENT,
+    idUsuario INT NOT NULL AUTO_INCREMENT,
 	usuario_email varchar(150) NOT NULL,
 	usuario_senha varchar(50) NOT NULL,
     usuario_nome varchar(150) NOT NULL,
     usuario_contato varchar(12) not null,
     usuario_endereco varchar(250) NOT NULL,
+    usuario_cargo varchar(45) check (usuario_cargo = 'administrador' or usuario_cargo = 'tecnico' or usuario_cargo = 'usuario'),
     primary key(idUsuario)
     );
-
-CREATE TABLE tecnicos(
-	idtecnico INT NOT NULL AUTO_INCREMENT,
-    tecnico_email varchar(150) NOT NULL,
-    tecnico_senha varchar(50) NOT NULL,
-    tecnico_nome varchar(150) NOT NULL,
-	tecnico_contato varchar(12) not null,
-    tecnico_endereco varchar(250) NOT NULL,
-    primary key(idtecnico)
-);
 
 CREATE TABLE Chamado(
 	idChamado INT NOT NULL AUTO_INCREMENT,
@@ -49,4 +30,3 @@ CREATE TABLE Chamado(
 
 Alter table Chamado ADD constraint fk_user FOREIGN KEY (idUsuario) REFERENCES Usuarios(idUsuario);
 Alter table Chamado ADD constraint fk_tecnico FOREIGN KEY (idtecnico) REFERENCES tecnicos(idtecnico);
-
